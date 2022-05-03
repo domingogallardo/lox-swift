f52d812ee840315ddafbdcff8cae4c2e49131d4a
Domingo Gallardo <domingo.gallardo@ua.es>
2022-05-03 12:30:27 +0200

    Añadido @main y struct principal

--- Sources/Lox/Lox.swift
+++ Sources/Lox/Lox.swift
@@ -1,39 +1,59 @@
 import Foundation
 
-var hadError = false
+@main
+struct Lox {
+    static var hadError = false
 
-func runFile(path: String) throws {
-    let url = URL(fileURLWithPath: path)
-    try run(source: String(contentsOf: url, encoding: .utf8))
-    if (hadError) {
-        exit(65)
-    } 
-}
-
-func runPrompt() {
-    while (true) {
-        print("> ", terminator: "") 
-        if let line = readLine() {
-            run(source: line)
-            hadError = false
+    static func main() {
+        if CommandLine.arguments.count > 2 {
+            print("Usage: jlox [script]")
         } else {
-            break
+            if (CommandLine.arguments.count == 2) {
+                do {
+                    try runFile(path: CommandLine.arguments[1])
+                } catch {
+                print("Unexpected error: \(error)")
+                exit(64)
+            }
+            } else {
+                runPrompt()
+            }
         }
     }
-}
 
-func run(source: String) {
-    let tokens = source.components(separatedBy: .whitespacesAndNewlines)
-    for token in tokens {
-        print(token)
+    static func runFile(path: String) throws {
+        let url = URL(fileURLWithPath: path)
+        try run(source: String(contentsOf: url, encoding: .utf8))
+        if (hadError) {
+            exit(65)
+        } 
     }
-}
 
-func error(line: Int, message: String) {
-    report(line: line, position: "", message: message)
-}
+    static func runPrompt() {
+        while (true) {
+            print("> ", terminator: "") 
+            if let line = readLine() {
+                run(source: line)
+                hadError = false
+            } else {
+                break
+            }
+        }
+    }
 
-func report(line: Int, position: String, message: String) {
-    print("[line \(line)] Error \(position): \(message)")
-    hadError = true
+    static func run(source: String) {
+        let tokens = source.components(separatedBy: .whitespacesAndNewlines)
+        for token in tokens {
+            print(token)
+        }
+    }
+
+    static func error(line: Int, message: String) {
+        report(line: line, position: "", message: message)
+    }
+
+    static func report(line: Int, position: String, message: String) {
+        print("[line \(line)] Error \(position): \(message)")
+        hadError = true
+    }
 }
